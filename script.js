0f47deb21b6d13037d93f1ab7e161a09033a3ff0
Sarah Fischer <sarah@codefellows.com>
2018-07-28 14:49:36 -0700

    update scroll features

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 400) {
-         $("header").addClass("changeColor")
-      }
-      if ($(this).scrollTop() < 400) {
-         $("header").removeClass("changeColor")
-      }
-   });
+// $(function() {
+//    $(window).scroll(function () {
+//       if ($(this).scrollTop() > 648) {
+//          $("header").addClass("changeColor")
+//       }
+//       if ($(this).scrollTop() < 648) {
+//          $("header").removeClass("changeColor")
+//       }
+//    });
+// });
+
+// $(document).ready(function(){
+//   $(window).scroll(function() {
+//     if ($("#about").scrollTop() == 0) {
+//       $("#about-link").addClass("underline")
+//     } else {
+//       $("#about-link").removeClass("underline");
+//     }
+//   });
+// });
+
+$(document).scroll(function(){
+    var st = $(this).scrollTop();
+
+    $("section").each(function() {
+        if(st >= $(this).offset().top && st <= $(this).offset().top + $(this).height()){
+            var id = $(this).attr('id');
+            $('a[href="#'+id+'"]').addClass('underline');
+        }else{
+            var id = $(this).attr('id');
+            $('a[href="#'+id+'"]').removeClass('underline');
+        }
+    });
+
+});
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top + 1
+    }, 500);
 });
