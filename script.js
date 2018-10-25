2638f78418bd02d5e93c2657bce34b175fba2d46
Sarah Fischer <sarah@codefellows.com>
2018-10-24 22:56:05 -0700

    finish css styles

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-$(document).scroll(function(){
-    var st = $(this).scrollTop();
-
-    $("section").each(function() {
-        if(st >= $(this).offset().top && st <= $(this).offset().top + $(this).height()){
-            var id = $(this).attr('id');
-            $('a[href="#'+id+'"]').addClass('underline');
-        }else{
-            var id = $(this).attr('id');
-            $('a[href="#'+id+'"]').removeClass('underline');
-        }
-    });
-});
 
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
